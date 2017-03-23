fe7aba360430a26a395db06cec4d62c85328d809
denglibing <denglibing@fangdd.com>
2017-03-23 18:41:18 +0800

    extionsion 使用open修饰

--- FDDUITableViewDemoSwift/FDDBaseRepo/FDDTableViewConverter.swift
+++ FDDUITableViewDemoSwift/FDDBaseRepo/FDDTableViewConverter.swift
@@ -28,11 +28,11 @@ extension UITableView {
 // 通过重载来实现特殊的cell
 extension FDDBaseTableViewController{
     
-    @objc(tableView:numberOfRowsInSection:) func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    @objc(tableView:numberOfRowsInSection:) open func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.dataArr.count
     }
     
-    @objc(tableView:cellForRowAtIndexPath:) func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    @objc(tableView:cellForRowAtIndexPath:) open func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cellModel: FDDBaseCellModel = self.dataArr.object(at: indexPath.row) as! FDDBaseCellModel
         let cell: FDDBaseTableViewCell = tableView.cellForIndexPath(indexPath, cellClass: cellModel.cellClass)!
         cell.setCellData(cellModel.cellData, delegate: self)
@@ -40,7 +40,7 @@ extension FDDBaseTableViewController{
         return cell
     }
     
-    @objc(tableView:heightForRowAtIndexPath:) func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    @objc(tableView:heightForRowAtIndexPath:) open func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         let cellModel: FDDBaseCellModel = self.dataArr.object(at: indexPath.row) as! FDDBaseCellModel
         return CGFloat(cellModel.cellHeight)
     }
